5ff1c629d0438fcf3a3b2de2b6e41a51466419c4
cr0hszard <alessandro.gasperini93@gmail.com>
2018-07-09 14:25:15 +0200

    Moved HTML , css files, controllers(angular)  and services(angular) in separate folders and files, added the search method to be able to search in the array of Cars by Year Id or Brand

--- CarAPP/CarAPP/Frontend/indexRouting.js
+++ CarAPP/CarAPP/Frontend/indexRouting.js
@@ -1,20 +1,20 @@
-﻿var Indexrouter = angular.module("indexRouting", ["ngRoute","CarApp","EditCar"]);
+﻿var Indexrouter = angular.module("indexRouting", ["ngRoute", "CarApp", "EditCar"]);
 //Router for the Index.htmlpage
 Indexrouter.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "HomePage.html"
-           
+            templateUrl: "Pages/HomePage.html"
+
         })
         .when("/addCar", {
-            templateUrl: "addCar.html",
-           controller: "AddCar"
+            templateUrl: "Pages/addCar.html",
+            controller: "AddCar"
         })
         .when("/listCar", {
 
-            templateUrl: "car.html",
+            templateUrl: "Pages/car.html",
             controller: "CarCtrl"
         }).otherwise({
-            redirectTo:"/"
+            redirectTo: "/"
         });
 });
